527636a18e50537fe59334eca3a8054e4c12d4e2
yann jebeh <gangstersyannslby@gmail.com>
2022-07-04 21:02:06 +0800

    A

--- index.js
+++ index.js
@@ -202,20 +202,20 @@ XeonBotInc.sendMessage(anu.id, buttonMessage, {quoted:unicorndoc})
                     const xeonmembers = metadata.participants.length
                     let unicorndoc = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "916909137213-1604595598@g.us"}, "message": {orderMessage: {itemCount: 9999999,status: 200, thumbnail: buffer, surface: 200, message: `${metadata.subject}`, orderTitle: 'xeon', sellerJid: '0@s.whatsapp.net'}}, contextInfo: {"forwardingScore":999,"isForwarded":true},sendEphemeral: true}
                     xeonbody = `┌─❖
-│「 𝗚𝗼𝗼𝗱𝗯𝘆𝗲 👋 」
+│「 sᴇʟᴀᴍᴀᴛ ᴛɪɴɢɢᴀʟ 👋 」
 └┬❖ 「 @${xeonName.split("@")[0]}  」
-   │✑  𝗟𝗲𝗳𝘁 
+   │✑  ᴋᴇʟᴜᴀʀ ɢʀᴜᴘ :
    │✑ ${metadata.subject}
-   │✑  𝗠𝗲𝗺𝗯𝗲𝗿 : 
+   │✑  ᴍᴇᴍʙᴇʀ ᴋᴇ : 
    │✑ ${xeonmembers}th
-   │✑  𝗧𝗶𝗺𝗲 : 
+   │✑  ᴡᴀᴋᴛᴜ : 
    │✑  ${xeontime} ${xeondate}
    └───────────────┈ ⳹`
       //if you copy the code value,
    //dont forget to put my name(Xeon) as credit
    //you fail to put, i sue you for sure!
 let buttons = [
-{buttonId: `wkwkwk`, buttonText: {displayText: 'Sayonara 🥀'}, type: 1}
+{buttonId: `wkwkwk`, buttonText: {displayText: 'sᴇʟᴀᴍᴀᴛ ᴛɪɴɢɢᴀʟ 🥀'}, type: 1}
 ]
 let buttonMessage = {
 document: fs.readFileSync('./XeonMedia/theme/cheems.xlsx'),
@@ -230,7 +230,7 @@ buttons: buttons,
 headerType: 4,
 contextInfo:{externalAdReply:{
 title: `${ownername}`,
-body: `Bye! my friend, take care.`,
+body: `sᴇʟᴀᴍᴀᴛ ᴛɪɴɢɢᴀʟ sᴇᴍᴏɢᴀ ᴅɪ ᴛᴇʀɪᴍᴀ ᴅɪ sɪsɪɴʏᴀ 🤗.`,
 mediaType:2,
 thumbnail: buffer,
 sourceUrl: `${websitex}`,
